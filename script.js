29ea67f857acc4a20992039778b2803e80e26877
leanndemetro <leanndemetro@gmail.com>
2020-09-17 22:09:20 -0400

    OG push

---
+++ script.js
@@ -0,0 +1,31 @@
+
+//creates a variable that contains the current date information via moment.js
+var currentDate = (moment().format("dddd, MMMM do"));
+$("currentDay").text(currentDate);
+
+//appends the currentDate variable to the #currentday ID element in html
+document.getElementById("currentDay").append(currentDate);
+
+
+//change time block background color depending on the current hour (past, present, or future)
+
+
+    //figure out how to grab current hour (8 blocks total)
+    console.log(moment().hour());
+
+
+
+
+
+
+
+
+//When app opens, display content for each time block from local storage
+    //it can be done with a loop (more efficient) or by targeting reach time block separately
+
+
+//click event to save text area content to local storage
+
+//When app opens, display content for each time block from local storage
+    //it can be done with a loop (more efficient) or by targeting reach time block separately
+
