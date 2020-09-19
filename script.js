8e43fd87d125a11c23d0d1ee15e4fdb83dceb359
leanndemetro <leanndemetro@gmail.com>
2020-09-18 23:16:22 -0400

    updated js

--- script.js
+++ script.js
@@ -1,17 +1,133 @@
 
+
+var currentHour = (moment().hour());
+console.log(currentHour);
+
 //creates a variable that contains the current date information via moment.js
-var currentDate = (moment().format("dddd, MMMM do"));
+var currentDate = (moment().format("dddd, MMMM Do"));
 $("currentDay").text(currentDate);
+
 
 //appends the currentDate variable to the #currentday ID element in html
 document.getElementById("currentDay").append(currentDate);
 
+//add textarea input to local storage
 
-//change time block background color depending on the current hour (past, present, or future)
+$("#saveBtn9").on("click", function () {
+    console.log($("#9").val());
+    localStorage.setItem("nineAm", $("#9").val())
+});
+$("#saveBtn10").on("click", function () {
+    localStorage.setItem("tenAm", $("#10").val())
+});
+$("#saveBtn11").on("click", function () {
+    localStorage.setItem("elevenAm", $("#11").val())
+});
+$("#saveBtn12").on("click", function () {
+    localStorage.setItem("twlevePm", $("#12").val())
+});
+$("#saveBtn1").on("click", function () {
+    localStorage.setItem("onePm", $("#1").val())
+});
+$("#saveBtn2").on("click", function () {
+    localStorage.setItem("twoPm", $("#2").val())
+});
+$("#saveBtn3").on("click", function () {
+    localStorage.setItem("threePm", $("#3").val())
+});
+$("#saveBtn4").on("click", function () {
+    localStorage.setItem("fourPm", $("#4").val())
+});
+$("#saveBtn5").on("click", function () {
+    localStorage.setItem("fivePm", $("#5").val())
+});
+
+//initializes page
+init();
+
+//function that checks if there are values stored in local storage and adds them to page upon initialization 
+function init() {
+    var textData = localStorage.getItem("nineAm");
+    if(textData !== null){
+        $("#9").text(textData);
+    }
+
+    textData = localStorage.getItem("tenAm");
+    if(textData !== null) {
+        $("#10").text(textData);
+    }
+    textData = localStorage.getItem("elevenAm");
+    if(textData !== null) {
+        $("#11").text(textData);
+    }
+    textData = localStorage.getItem("twelvePm");
+    if(textData !== null) {
+        $("#12").text(textData);
+    }
+    textData = localStorage.getItem("onePm");
+    if(textData !== null) {
+        $("#1").text(textData);
+    }
+    textData = localStorage.getItem("twoPm");
+    if(textData !== null) {
+        $("#2").text(textData);
+    }
+    textData = localStorage.getItem("ThreePm");
+    if(textData !== null) {
+        $("#3").text(textData);
+    }
+    textData = localStorage.getItem("fourPm");
+    if(textData !== null) {
+        $("#4").text(textData);
+    }
+    textData = localStorage.getItem("fivePm");
+    if(textData !== null) {
+        $("#5").text(textData);
+    }
+}
+
+var nineMt = document.getElementById("9").value = 9;
+
+console.log(nineMt);
+
+var tenMt = document.getElementById("10").value = 10;
+
+console.log(tenMt);
+
+var elevenMt = document.getElementById("10").value = 11;
+
+console.log(elevenMt);
+
+var twelveMt = document.getElementById("12").value = 12;
+
+console.log(twelveMt);
+
+var oneMt = document.getElementById("wun").value = 13;
+
+console.log(oneMt);
+
+var twoMt = document.getElementById("2").value = 14;
+
+console.log(twoMt);
+
+var threeMt = document.getElementById("3").value = 15;
+
+console.log(threeMt);
+
+var fourMt = document.getElementById("4").value = 16;
+
+console.log(fourMt);
+
+var fiveMt = document.getElementById("5").value = 17;
+
+console.log(fiveMt);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
 
 
-    //figure out how to grab current hour (8 blocks total)
-    console.log(moment().hour());
 
 
 
@@ -20,12 +136,5 @@ document.getElementById("currentDay").append(currentDate);
 
 
 
-//When app opens, display content for each time block from local storage
-    //it can be done with a loop (more efficient) or by targeting reach time block separately
 
-
-//click event to save text area content to local storage
-
-//When app opens, display content for each time block from local storage
-    //it can be done with a loop (more efficient) or by targeting reach time block separately
 
